ec5c0e7034598e67029a49bcae0226fd93f6ff7b
SergeyErshov <s.ershov.shoko@gmail.com>
2022-11-21 14:02:04 +0300

    formated loadbalancer.tf

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -3,16 +3,16 @@
 # Создаем целевую группу web_balancer
 
 resource "yandex_lb_target_group" "web_balancer" {
-   name = "target_group_1"
-   region_id = var.ya_region
+  name      = "target_group_1"
+  region_id = var.ya_region
 
-   target {
-      subnet_id = module.yandex_instance_1.vpc_subnet
-      address = module.yandex_instance_1.external_ipaddr_vm
-   }
-   
-   target {
-       subnet_id = module.yandex_instance_2.vpc_subnet
-       address = module.yandex_instance_2.external_ipaddr_vm
+  target {
+    subnet_id = module.yandex_instance_1.vpc_subnet
+    address   = module.yandex_instance_1.external_ipaddr_vm
+  }
+
+  target {
+    subnet_id = module.yandex_instance_2.vpc_subnet
+    address   = module.yandex_instance_2.external_ipaddr_vm
   }
 }

584a3757e51840382f2651abcd0b5317573f97fd
SergeyErshov <s.ershov.shoko@gmail.com>
2022-11-21 14:24:39 +0300

    some changes in syntax loadbalancer.tf

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -8,11 +8,11 @@ resource "yandex_lb_target_group" "lb_tg_01" {
 
   target {
     subnet_id = module.yandex_instance_1.vpc_subnet
-    address   = module.yandex_instance_1.external_ipaddr_vm
+    address   = module.yandex_instance_1.internal_ipaddr_vm
   }
 
   target {
     subnet_id = module.yandex_instance_2.vpc_subnet
-    address   = module.yandex_instance_2.external_ipaddr_vm
+    address   = module.yandex_instance_2.internal_ipaddr_vm
   }
 }
